14cea1cf66d604bfb5e1849df530e617afa21ced
Brandon Pugh <bp@brandonpugh.com>
2016-02-08 11:45:00 -0600

    Use chokidar to watch for new files being added

--- src/archiver.js
+++ src/archiver.js
@@ -1,31 +1,32 @@
 const fs = require('fs');
+const chokidar = require('chokidar');
 const path = require('path');
 const spawn = require('child_process').spawn;
 const vidsdir = 'C:\\Users\\Brandon\\Dropbox\\projects\\vids\\test\\';
 const archiveDir = 'C:\\Users\\Brandon\\Dropbox\\projects\\vids\\archive\\';
 const errorDir = 'C:\\Users\\Brandon\\Dropbox\\projects\\vids\\error\\';
 
-// const files = fs.readdirSync('C:\\Users\\Brandon\\Dropbox\\projects\\vids');
-// files.foreach(function(file) {
-//   console.log(file);
-// });
-
-// todo: use sync version for simplicity
-fs.readdir(vidsdir, function(err, files) {
-  if (err) throw err;
-  if (files.length < 1) console.log('No files found.');
-  files.forEach(function(file) {
-    const filePath = vidsdir + file;
-    const ext = path.extname(filePath);
-    if (ext === '.txt') {
-      fs.readFile(filePath, {encoding: 'utf-8'}, function(err2, data) {
-        console.log(file);
-        console.log('data: ' + data);
-        downloadVid(data, filePath);
-      });
-    }
-  });
+const watcher = chokidar.watch(vidsdir, {
+  ignored: /[\/\\]\./,
+  persistent: true,
 });
+watcher
+  .on('add', newpath => {
+    console.log(`File ${newpath} has been added`);
+    readFile(newpath);
+  });
+
+function readFile(file) {
+  const filePath = path.isAbsolute(file) ? file : vidsdir + file;
+  const ext = path.extname(filePath);
+  if (ext === '.txt') {
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err2, data) {
+      console.log(file);
+      console.log('data: ' + data);
+      downloadVid(data, filePath);
+    });
+  }
+}
 
 function downloadVid(url, filePath) {
   const downloaddir = 'C:\\temp\\';
