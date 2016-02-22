ae54dfda018ab78e0bff4f2f3a03c321c0f74482
Brandon Pugh <bp@brandonpugh.com>
2016-02-22 00:05:19 -0600

    Use personal eslint config package

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
-const fs = require('fs');
-const chokidar = require('chokidar');
-const path = require('path');
-const spawn = require('child_process').spawn;
-const rc = require('rc');
-const appName = 'vidArchiver';
-const config = rc(appName, {});
-console.log(config);
+const fs = require('fs')
+const chokidar = require('chokidar')
+const path = require('path')
+const spawn = require('child_process').spawn
+const rc = require('rc')
+const appName = 'vidArchiver'
+const config = rc(appName, {})
+console.log(config)
 
 const watcher = chokidar.watch(config.vidsdir, {
   ignored: /[\/\\]\./,
   persistent: true,
-});
+})
 watcher
   .on('add', newpath => {
-    console.log(`File ${newpath} has been added`);
-    readFile(newpath);
-  });
+    console.log(`File ${newpath} has been added`)
+    readFile(newpath)
+  })
 
 function readFile(file) {
-  const filePath = path.isAbsolute(file) ? file : config.vidsdir + file;
-  const ext = path.extname(filePath);
+  const filePath = path.isAbsolute(file) ? file : config.vidsdir + file
+  const ext = path.extname(filePath)
   if (ext === '.txt') {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err2, data) {
-      console.log(file);
-      console.log('data: ' + data);
-      downloadVid(data, filePath);
-    });
+      console.log(file)
+      console.log('data: ' + data)
+      downloadVid(data, filePath)
+    })
   }
 }
 
 function downloadVid(url, filePath) {
-  const outputPath = config.downloadDir + '%(title)s.%(ext)s';
-  const yt = spawn('youtube-dl', ['-o', outputPath, url]);
+  const outputPath = config.downloadDir + '%(title)s.%(ext)s'
+  const yt = spawn('youtube-dl', ['-o', outputPath, url])
   yt.stdout.on('data', function(data) {
-    console.log('stdout: ' + data);
-  });
+    console.log('stdout: ' + data)
+  })
   yt.stderr.on('data', function(data) {
-    console.error('stderr: ' + data);
-  });
+    console.error('stderr: ' + data)
+  })
   yt.on('close', function(code) {
-    const filename = path.basename(filePath);
+    const filename = path.basename(filePath)
     if (code === 0) { // success
-      fs.renameSync(filePath, config.archiveDir + filename);
-      console.log(`${filename} successfully archived`);
+      fs.renameSync(filePath, config.archiveDir + filename)
+      console.log(`${filename} successfully archived`)
     } else { // error
-      fs.renameSync(filePath, config.errorDir + filename);
-      console.error(`Error archiving ${filename}`);
+      fs.renameSync(filePath, config.errorDir + filename)
+      console.error(`Error archiving ${filename}`)
     }
-  });
+  })
 }
